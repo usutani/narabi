1e6565aed58c3565e553067415241bc432a41979
Yasuhiro Usutani <y_usutani@me.com>
2011-11-17 17:44:30 +0900

    ヘルプを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Narabi::Application.routes.draw do
 
   resources :instances
 
+  match 'help' => 'help#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
