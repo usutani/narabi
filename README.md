43b7ad4ddec53ef5254974da2e5edddf27142fb8
Yasuhiro Usutani <y_usutani@me.com>
2013-04-17 22:12:15 +0900

    著作権を2013年までに更新

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # narabi
 
-Copyright (c) 2011,2012 Technical Group Laboratory, Inc. See LICENSE for details.
+Copyright (c) 2011-2013 Technical Group Laboratory, Inc. See LICENSE for details.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607075703) do
+ActiveRecord::Schema.define(:version => 20130417002901) do
 
   create_table "diagrams", :force => true do |t|
     t.string   "title"
@@ -39,5 +39,15 @@ ActiveRecord::Schema.define(:version => 20120607075703) do
     t.boolean  "is_note"
     t.integer  "diagram_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
