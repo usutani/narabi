c03641607d7d6e66863e1cd1eb85d312e3c2193d
Yasuhiro Usutani <y_usutani@me.com>
2013-06-07 22:51:46 +0900

    MySQL long text

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,10 @@ ActiveRecord::Schema.define(:version => 20130417002901) do
   end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "session_id",                       :null => false
+    t.text     "data",       :limit => 4294967295
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
